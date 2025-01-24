51d49863001886510b2cdd80ee20da27c6b60663
mrlakakk <mrlakakk@gmail.com>
2025-01-24 12:48:19 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,6 @@
 {
-"megafile" : "https://mega.nz/file/UwQFmTBL#l3ULwBOu05NUdOjDR1eUKQpAt_gnDa2m1v38fvyRiaM"
+    "zip":"https://mega.nz/file/UwQFmTBL#l3ULwBOu05NUdOjDR1eUKQpAt_gnDa2m1v38fvyRiaM",
+    "beta" : "https://mega.nz/file/UwQFmTBL#l3ULwBOu05NUdOjDR1eUKQpAt_gnDa2m1v38fvyRiaM",
+    "ponnaya" : "https://mega.nz/file/aqZEiZ4a#rUU8_zJMU32Rn-zq_1YNSDdOe6DBbHHXX3APTksxiyA",
+    "moviefree": false
 }
