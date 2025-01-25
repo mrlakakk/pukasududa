fdb015ebed5f3529e2df9930a40183b2ac885a02
mrlakakk <mrlakakk@gmail.com>
2025-01-25 19:03:05 +0530

    Update lakaofc.js

--- pakaya/lakaofc.js
+++ pakaya/lakaofc.js
@@ -1,1 +1,22 @@
+const { cmd } = require('../command');
 
+cmd({
+    pattern: "repo",
+    alias: ["mrlaka", "sc", "github", "botinfo", "info", "deploy", "bot"],
+    react: "🌟",
+    desc: "Check bot owner.",
+    category: "main",
+    filename: __filename
+}, async (conn, mek, m, { from, reply }) => {
+    try {
+        const cap = "Hello Hutto";
+        await conn.sendMessage(from, { react: { text: "🌟", key: mek.key } });
+        return await conn.sendMessage(from, {
+            image: { url: "https://raw.githubusercontent.com/lakaofc/LAKAMD-DATABASE/refs/heads/main/img/laka.jpg" },
+            caption: cap
+        }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`Error: ${e.message}`);
+    }
+});
