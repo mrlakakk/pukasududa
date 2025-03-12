39c8a02eeaf5e138a28b1ca3a5d541c88dc2a4f4
mrlakakk <mrlakakk@gmail.com>
2025-03-12 18:35:47 +0530

    Add files via upload

---
+++ textdata.js
@@ -0,0 +1,18 @@
+{
+"footer": "> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴀᴋᴀ-ᴍᴅ",
+"mainimg": "https://i.ibb.co/DfzLTBvB/image-1741179875839.jpg",
+"version": "2.0.0",
+"barkmdg": "අපිට කොහෙද මාලු අපි යන්නේ නැනේ මුහුදු",
+"alivemg": "❄️",
+"replyMsg": {
+  "givetext": "*Please give me text..!✏️*",
+  "giveurl": "*Please give me Url..!🖇️*",
+  "cowner": "*Owner Only Comade..!🪄*",
+  "gadm": "*Give Me Group Admin..!🫣*",
+  "notfoud": "*Nof Dwnd..!*",
+  "gonly": "*This Comade Only Group..!👥*",
+  "pinbox": "*This Comade Only Use Inbox..!👁️*",
+  "emsg": "*ERORR..!*"
+
+}
+}
