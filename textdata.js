71e05acebdd820f9e7160a2f70fa4e6983e02cc0
mrlakakk <mrlakakk@gmail.com>
2025-04-01 22:57:24 +0530

    Update textdata.js

--- textdata.js
+++ textdata.js
@@ -1,6 +1,6 @@
 {
 "footer": "> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴀᴋᴀ-ᴍᴅ",
-"mainimg": "https://i.ibb.co/DfzLTBvB/image-1741179875839.jpg",
+"mainimg": "https://i.ibb.co/HLBcwR8r/image-1743526150546.jpg",
 "version": "2.0.0",
 "barkmdg": "අපිට කොහෙද මාලු අපි යන්නේ නැනේ මුහුදු",
 "alivemg": "*Bot Deploy New Detels 🔥*\n\n*Repo 🔗 :*\n\nhttps://github.com/lakabbh/new\n\n*web (pair web) 🌐:*\n\nhttps://lakaweb-58ee184d3525.herokuapp.com/\n\n*Heaku Deploy Link : 💐*\n\nhttps://dashboard.heroku.com/new?template=https://github.com/lakabbh/new\n\n*Bot Zip Files 📂 :*\n\nhttps://www.mediafire.com/file/mn1xb2xi3rk6q7a/Laka+Md+V2.zip/file",
